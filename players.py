fb5c1321897463b5fc82d5290a52dae9639d93fb
Ryan Fox <ryan@foxrow.com>
2017-03-28 21:02:31 -0500

    added GreedyPlayer

--- players.py
+++ players.py
@@ -5,6 +5,25 @@ class Player:
     def play(self, *args, **kwargs):
         raise NotImplementedError
 
+    @staticmethod
+    def is_winning(game, position):
+        return game.winner() == position + 1
+
+
+class GreedyPlayer(Player):
+    """
+    Attempts to play a greedy strategy: if not currently winning, plays 4/5.
+    If currently winning, plays 1/8.
+    """
+    def __init__(self):
+        self.name = 'Greedy'
+
+    def play(self, game=None, position=None):
+        if self.is_winning(game, position):
+            return random.choice([1, 8])
+
+        return random.choice([4, 5])
+
 
 class RandomPlayer(Player):
     def __init__(self):
@@ -23,12 +42,13 @@ class ConstantPlayer(Player):
         return self.constant
 
 
-class ScorePlusTwoPlayer(Player):
-    def __init__(self):
-        self.name = 'Score+2'
+class ScorePlusPlayer(Player):
+    def __init__(self, plus=2):
+        self.name = f'Score+{plus}'
+        self.plus = plus
 
     def play(self, game=None, position=None):
-        return min(8, game.get_score()[position] + 2)
+        return min(8, game.get_score()[position] + self.plus)
 
 
 class HumanPlayer(Player):
